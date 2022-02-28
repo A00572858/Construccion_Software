8f435f5aa22361e0addc70ecab1a6f49fb9a1fb3
A00572858 <A00572858@itesm.mx>
2022-02-28 10:45:53 -0600

    Laboratorio 8

--- Laboratorios/Lab4/Laboratorio4.js
+++ Laboratorios/Lab4/Laboratorio4.js
@@ -1,3 +1,4 @@
+
 function tabla_cuadrados(num) {
     let resultado = "<table>";
     for (let i = 1; i <= num; i++) {
@@ -9,9 +10,82 @@ function tabla_cuadrados(num) {
     return resultado;
 }
 
-document.getElementById("TablaCuadrados").onclick = () => {
-    let resp1 = prompt("Escribe un número menor a 20: ");
-    document.getElementById("cuadrados").innerHTML = tabla_cuadrados(resp1);
+function suma (){
+    a = Math.floor(Math.random() * 100);
+    b = Math.floor(Math.random() * 100);
+    let r = prompt("¿Cuál es el resultado de la suma: "+a+" + "+b+"?");
+    let resp = "";
+    if (r == (a+b)){
+        resp = "Correcto, la respuesta es " + r;
+    }
+    else{
+        resp = "Incorrecto, la respuesta es " + (a+b);
+    }
+    return resp;
 }
 
+function contador(array){
+    let ceros = 0;
+    let positivos = 0;
+    let negativos = 0;
+    for (let i = 0; i < array.length; i++){
+        if (array[i] == 0){
+            ceros += 1;
+        }
+        else if (array[i] < 0){
+            negativos += 1;
+        }
+        else {
+            positivos += 1;
+        }
+    }
+    s = "<table><tr><td>Ceros:</td><td>"+ceros+"</td></tr><tr><td>Positivos:</td><td>"+positivos+"</td></tr><tr><td>Negativos:</td><td>"+negativos+"</td></tr></table>"
+    return s;
+}
 
+function promedios (matrix){
+    let r = "<table>";
+    let suma = 0;
+    for (let i = 0; i<matrix.length; i++){
+        r += "<tr><td>El promedio de la fila numero "+(i+1)+"= </td><td>";
+        for (var j = 0; j<matrix[i].length; j++){
+            suma += matrix[i][j];
+        }
+        r += (suma/(j+1)).toFixed(2) + "</td></tr>";
+        suma = 0;
+    }
+    r += "</table>";
+    return r;
+}
+
+function inverso (num){
+    let r = "";
+    for (let i = (num.length-1); i > -1  ; i--){
+        r += num[i];
+    }
+    return r;
+}
+
+document.getElementById("Problema1").onclick = () => {
+    let resp1 = prompt("Escribe un número menor a 20: ");
+    document.getElementById("p1").innerHTML = tabla_cuadrados(resp1);
+}
+
+document.getElementById("Problema2").onclick = () => {
+    document.getElementById("p2").innerHTML = suma();
+}
+
+document.getElementById("Problema3").onclick = () => {
+    let resp3 = [-2,-1,0,0,1,2,3,0,-4];
+    document.getElementById("p3").innerHTML = contador(resp3);
+}
+
+document.getElementById("Problema4").onclick = () => {
+    let resp4 = [[1,3,5,7,9],[2,4,6,8,10],[1,2,3,5,7,11,13],[3,1,4,1,5,9,2,6]]
+    document.getElementById("p4").innerHTML = promedios(resp4);
+}
+
+document.getElementById("Problema5").onclick = () => {
+    let resp5 = prompt("Escribe un número de más de dos dígitos: ");
+    document.getElementById("p5").innerHTML = inverso(resp5);
+}
---
+++ Laboratorios/Lab8/Laboratorio8.js
@@ -0,0 +1,59 @@
+const filesystem = require('fs');
+const rl = require('readline').createInterface({input : process.stdin, output: process.stdout});
+
+function arreglo(array) {
+    let suma = 0;
+    for (var i in array){
+        suma += array[i];
+    }
+    return suma/(Number(i)+1)
+}
+
+function showarray () {
+    let array = [];
+    console.log("Tamaño de array = 5")
+        console.log('Los valores del array son: ');
+        for (let j = 0; j < 5; j++){
+            num = (j+1)*2;
+            console.log(num);
+            array.push(num);
+        }
+    console.log('El promedio del array es:');
+    console.log(arreglo(array));
+}
+
+function escribirstring () {
+    console.log('El string a guardar en el archivo string.txt es: \nEste archivo es demasiado corto, pero bueno.');
+    let string = "Este archivo es demasiado corto, pero bueno.";
+        filesystem.writeFileSync('string.txt',string);
+        console.log('Archivo creado con exito.');
+}
+
+function fibonacci (n, a) {
+    if (n in a){return a[n]}
+    if (n <=2){return 1}
+    else{
+        a[n] = fibonacci(n-1,a) + fibonacci(n-2,a);
+        return a[n]
+    }
+}
+
+function showfibo() {
+    let a = {};
+    console.log("Fibonacci number to test = 50 \n(50th iteration of the sequence)");
+    console.log(fibonacci(50,a));
+}
+
+
+const http = require("http");
+const server = http.createServer((request,response) => {
+    response.setHeader('Content-Type','text/html');
+    let string = filesystem.readFileSync('Construccion_software/Laboratorios/Lab8/Laboratorio1.html', 'utf-8');
+    response.write(string);
+    response.end();
+});
+
+showarray();
+escribirstring();
+showfibo();
+server.listen(3000);
